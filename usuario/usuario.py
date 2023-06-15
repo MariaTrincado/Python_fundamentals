0ab04892e5dcc8248e4077003cacff7572e07dd3
María Trincado <maria_trincado@hotmail.com>
2023-06-14 23:00:02 -0400

    Vie9_Sem4

--- usuario/usuario.py
+++ usuario/usuario.py
@@ -15,14 +15,17 @@ class Usuario:
             print("Saldo insuficiente para hacer el retiro")
 
 
-    def hacer_deposito(self, otro_usuario, monto):
-        # Lógica para hacer depósito a otro usuario
-        return None
+    def hacer_deposito(self, monto):
+        if monto > 0:
+            self.balance += monto
+            print(f"Se realizó un depósito de ${monto}. Nuevo balance: ${self.balance}")
+        else:
+            print("El monto depositado debe ser mayor a $0")
 
     def mostrar_balance_usuario(self):
         print(f"Usuario: {self.nombre}, su balance es de ${self.balance}")
     
-    def hacer_deposito(self, otro_usuario, monto):
+    def hacer_transferencia(self, otro_usuario, monto):
         if monto <= self.balance:
             self.balance -= monto
             otro_usuario.balance += monto
@@ -39,8 +42,24 @@ persona2 = Usuario("Monty Mekano", "monty@python.com", balance = 2000)
 persona3 = Usuario("Cristina Tank", "cristina@python.com",  balance = 500)
 
 # Llamadas a métodos
-persona1.hacer_retiro(monto=100)
-persona2.hacer_deposito(otro_usuario=persona3, monto=50)
+persona1.hacer_deposito(monto=100)
+persona1.hacer_deposito(monto=60)
+persona1.hacer_deposito(monto=40)
+persona1.hacer_retiro(monto=300)
 persona1.mostrar_balance_usuario()
+
+persona2.hacer_deposito(monto=100)
+persona2.hacer_deposito(monto=80)
+persona2.hacer_retiro(monto=500)
+persona2.hacer_retiro(monto=130)
 persona2.mostrar_balance_usuario()
+
+persona3.hacer_deposito(monto=100)
+persona3.hacer_retiro(monto=60)
+persona3.hacer_retiro(monto=140)
+persona3.hacer_retiro(monto=400)
+persona3.mostrar_balance_usuario()
+
+persona1.hacer_transferencia(otro_usuario=persona3, monto=50)
+persona1.mostrar_balance_usuario()
 persona3.mostrar_balance_usuario()
